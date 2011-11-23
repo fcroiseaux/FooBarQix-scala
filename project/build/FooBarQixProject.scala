2547cc8432ea0a46e0cc10559c30e1af52c243ea
Fabrice Croiseaux <fcroiseaux@gmail.com>
2011-11-23 06:23:36 +0100

    compatibility with sbt 0.11

--- project/build/FooBarQixProject.scala
+++
@@ -1,6 +0,0 @@
-import sbt._ 
-
-class FooBarQixProject(info: ProjectInfo) extends DefaultProject(info) { 
-  val scalaToolsSnapshots = ScalaToolsSnapshots
-  val scalatest = "org.scalatest" %% "scalatest" % "1.6.1" % "test"
-}
