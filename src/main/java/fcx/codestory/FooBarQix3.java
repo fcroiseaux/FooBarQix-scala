a0cc1b0d65ddf483442ec745f7f8e5d3cfbaa475
Fabrice Croiseaux <fcroiseaux@gmail.com>
2011-11-30 04:42:24 +0100

    Ajout d'une version java

--- src/main/java/fcx/codestory/FooBarQix3.java
+++ src/main/java/fcx/codestory/FooBarQix3.java
@@ -8,4 +8,31 @@ package fcx.codestory;
  * To change this template use File | Settings | File Templates.
  */
 public class FooBarQix3 {
+    /*
+  * norm retourne la chaine s si i est egal a 0, retourne une chaine vide sinon
+  */
+    public static String norm(String s, int i) {
+        int r = (1 / (1 + Math.abs(i)));
+        return s.substring((1 - r) * s.length());
+    }
+
+    public static String convert(int i) {
+        String s = norm("Foo", i % 3); // i est divible par 3
+        s = s + norm("Bar", i % 5); // i est divible par 5
+        s += norm("Qix", i % 7); // i est divible par 7
+        s += norm("Foo", (i / 10 - 3) % 10); // i commence par 3
+        s += norm("Bar", (i / 10 - 5) % 10); // i commence par 5
+        s += norm("Qix", (i / 10 - 7) % 10); // i commence par 7
+        s += norm("Foo", (i - 3) % 10); // i se termine par 3
+        s += norm("Bar", (i - 5) % 10); // i se termine par 5
+        s += norm("Qix", (i - 7) % 10); // i se termine par 7
+        return norm(String.valueOf(i), s.length()) + s; // si s.size = 0 on retourne i, s sinon
+    }
+
+    public static void main(String[] s) {
+        for (int i = 1; i < 100; i++) {
+            System.out.println(convert(i));
+        }
+    }
+
 }
--- src/test/scala/FooBarQixSpec.scala
+++ src/test/scala/FooBarQixSpec.scala
@@ -1,5 +1,6 @@
 import org.scalatest.Spec
 import fcx.codestory.FooBarQix2
+import fcx.codestory.FooBarQix3
 
 class FooBarQixSpec extends Spec {
 
@@ -55,5 +56,56 @@ class FooBarQixSpec extends Spec {
     }
   }
 
+  describe("FooBarQix3 java") {
+
+    it("1 --> 1") {
+      assert(FooBarQix3.convert(1) == ("1"))
+    }
+    it("2 --> 2") {
+      assert(FooBarQix3.convert(2) == ("2"))
+    }
+    it("3 --> FooFoo") {
+      assert(FooBarQix3.convert(3) == ("FooFoo"))
+    }
+    it("4 --> 4") {
+      assert(FooBarQix3.convert(4) == ("4"))
+    }
+    it("5 --> BarBar") {
+      assert(FooBarQix3.convert(5) == ("BarBar"))
+    }
+    it("6 --> Foo") {
+      assert(FooBarQix3.convert(6) == ("Foo"))
+    }
+    it("7 --> QixQix") {
+      assert(FooBarQix3.convert(7) == ("QixQix"))
+    }
+    it("8 --> 8") {
+      assert(FooBarQix3.convert(8) == ("8"))
+    }
+    it("9 --> Foo") {
+      assert(FooBarQix3.convert(9) == ("Foo"))
+    }
+    it("10 --> Bar") {
+      assert(FooBarQix3.convert(10) == ("Bar"))
+    }
+    it("13 --> Foo") {
+      assert(FooBarQix3.convert(13) == ("Foo"))
+    }
+    it("15 --> FooBarBar") {
+      assert(FooBarQix3.convert(15) == ("FooBarBar"))
+    }
+    it("21 --> FooQix") {
+      assert(FooBarQix3.convert(21) == ("FooQix"))
+    }
+    it("33 --> FooFooFoo") {
+      assert(FooBarQix3.convert(33) == ("FooFooFoo"))
+    }
+    it("51 --> FooBar") {
+      assert(FooBarQix3.convert(51) == ("FooBar"))
+    }
+    it("53 --> BarFoo") {
+      assert(FooBarQix3.convert(53) == ("BarFoo"))
+    }
+  }
 
 }
