7c9a5a5c49845311e1ade7187180b2a34a20412d
Fabrice Croiseaux <fcroiseaux@gmail.com>
2011-11-30 04:36:35 +0100

    Ajout d une version java avec les memes objectifs de l algorithme

---
+++ src/main/java/fcx/codestory/FooBarQix3.java
@@ -0,0 +1,11 @@
+package fcx.codestory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: croiseaux
+ * Date: 29/11/11
+ * Time: 22:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class FooBarQix3 {
+}
