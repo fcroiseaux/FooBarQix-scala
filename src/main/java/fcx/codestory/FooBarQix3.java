28672c84472385c813460e1a6880ffafc7aa8bad
fcroiseaux <fcroiseaux@gmail.com>
2011-12-02 16:12:46 +0100

    Amélioration de la généricité: - Possibilité de fixer un nombre d'itérations > 100 quelconque - Possibilité d'ajouter d'autres substitutions de mots pour d'autres valeurs qui respecteraient les même règles.

--- src/main/java/fcx/codestory/FooBarQix3.java
+++ src/main/java/fcx/codestory/FooBarQix3.java
@@ -1,38 +1,31 @@
 package fcx.codestory;
 
-/**
- * Created by IntelliJ IDEA.
- * User: croiseaux
- * Date: 29/11/11
- * Time: 22:36
- * To change this template use File | Settings | File Templates.
- */
 public class FooBarQix3 {
+
+    static String[] rpl = {"", "", "", "Foo", "", "Bar", "", "Qix", "", ""};
+
     /*
-  * norm retourne la chaine s si i est egal a 0, retourne une chaine vide sinon
-  */
+    * norm retourne la chaine s si i est egal a 0, retourne une chaine vide sinon
+    */
     private static final String norm(String s, int i) {
         int r = (1 / (1 + Math.abs(i)));
         return s.substring((1 - r) * s.length());
     }
 
     public static final String convert(int i) {
-        String s = norm("Foo", i % 3); // i est divible par 3
-        s = s + norm("Bar", i % 5); // i est divible par 5
-        s += norm("Qix", i % 7); // i est divible par 7
-        s += norm("Foo", (i / 10 - 3) % 10); // i commence par 3
-        s += norm("Bar", (i / 10 - 5) % 10); // i commence par 5
-        s += norm("Qix", (i / 10 - 7) % 10); // i commence par 7
-        s += norm("Foo", (i - 3) % 10); // i se termine par 3
-        s += norm("Bar", (i - 5) % 10); // i se termine par 5
-        s += norm("Qix", (i - 7) % 10); // i se termine par 7
+        String s = "";
+        for (int nb = 1; nb < 10; nb++)
+            s += norm(rpl[nb], i % nb);
+        for (int exp = 1; exp >= 0; exp--)
+            for (int nb = 1; nb < 10; nb++)
+                s += norm(rpl[nb], (i / ((int) Math.pow(10, exp)) - nb) % 10);    // i contient nb en exp ème position
         return norm(String.valueOf(i), s.length()) + s; // si s.size = 0 on retourne i, s sinon
     }
 
+
     public static void main(String[] s) {
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 100; i++)
             System.out.println(convert(i));
-        }
     }
 
 }
