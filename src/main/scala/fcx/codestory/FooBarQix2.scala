c7b5aeb19235ec1c0df95a0a1bf8a126a2478abd
fcroiseaux <fcroiseaux@gmail.com>
2011-12-02 16:11:33 +0100

    Amélioration de la généricité: - Possibilité de fixer un nombre d'itérations > 100 quelconque - Possibilité d'ajouter d'autres substitutions de mots pour d'autres valeurs qui respecteraient les même règles.

--- src/main/scala/fcx/codestory/FooBarQix2.scala
+++ src/main/scala/fcx/codestory/FooBarQix2.scala
@@ -2,25 +2,27 @@ package fcx.codestory
 
 object FooBarQix2 extends App {
 
+  foobarqix(100)
+
+  def rpl = List("", "", "", "Foo", "", "Bar", "", "Qix", "", "", "")
   /*
   * norm retourne la chaine s si i est egal a 0, retourne une chaine vide sinon
   */
   def norm(s: String, i: Int): String = s * (1 / (1 + i.abs))
 
-    def convert(i: Int): String = {
-    var s = norm("Foo", i % 3) // i est divible par 3
-    s += norm("Bar", i % 5) // i est divible par 5
-    s += norm("Qix", i % 7) // i est divible par 7
-    s += norm("Foo", (i / 10 - 3) % 10) // i commence par 3
-    s += norm("Bar", (i / 10 - 5) % 10) // i commence par 5
-    s += norm("Qix", (i / 10 - 7) % 10) // i commence par 7
-    s += norm("Foo", (i - 3) % 10) // i se termine par 3
-    s += norm("Bar", (i - 5) % 10) // i se termine par 5
-    s += norm("Qix", (i - 7) % 10) // i se termine par 7
-    norm(i.toString(), s.size) + s // si s.size = 0 on retourne i, s sinon   
+  def convert(i: Int): String = {
+    var s = ""
+    for (nb <- 1 to 9)
+      s += norm(rpl(nb), i % nb) // i est divible par n
+    for (exp <- math.log10(i).toInt to 0 by -1)
+      for (nb <- 1 to 9)
+        s += norm(rpl(nb), ((i / ((math.pow(10, exp)).toInt)) - nb) % 10) // i contient nb en exp ème position
+    norm(i.toString(), s.size) + s // si s.size = 0 on retourne i, s sinon
   }
 
-  for (i <- 1 to 100) {
-    Console.println(convert(i))
+  def foobarqix(max: Int) {
+    for (i <- 1 to max)
+      Console.println(convert(i))
   }
+
 }
